8d9bbfa188e5109e93e9feee234fbe7a1eb1d558
gmnawab <gmnawab25@gmail.com>
2024-06-08 21:52:08 +0600

    created the category section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../components/Banner";
+import Category from "../components/Category";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Category></Category>
         </div>
     );
 };
