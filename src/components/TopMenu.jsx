bb23edf896944740f2ec2077c267a0c724dba6ab
gmnawab <gmnawab25@gmail.com>
2024-06-12 09:28:22 +0600

    created custom hook to create custom menu section

--- src/components/TopMenu.jsx
+++ src/components/TopMenu.jsx
@@ -1,34 +1,28 @@
-import { useEffect } from "react";
 import SectionTitle from "./SectionTitle";
-import { useState } from "react";
 import TopMenuStyle from "./TopMenuStyle";
+import useMenu from "../hooks/useMenu";
 
 const TopMenu = () => {
-    const [menuData, setMenuData] = useState([]);
-
-    useEffect(()=> {
-        fetch('menu.json')
-        .then(res => res.json())
-        .then(data => {
-            const pupularMenu = data.filter(menu => menu.category === 'popular')
-            setMenuData(pupularMenu);
-        })
-    }, []);
+    const [menu] = useMenu();
+    const popularData = menu.filter(singleMenu => singleMenu.category === 'popular');
 
     return (
         <div className="pb-24">
             <SectionTitle
-            subHeading='Check it out'
-            heading='From our menu'
+                subHeading='Check it out'
+                heading='From our menu'
             ></SectionTitle>
 
             <div className="grid md:grid-cols-2 gap-8 mt-10">
                 {
-                    menuData.map(menu => <TopMenuStyle 
+                    popularData.map(menu => <TopMenuStyle
                         key={menu._id}
                         menu={menu}></TopMenuStyle>)
                 }
             </div>
+           <div className="flex justify-center mt-4">
+           <button className="btn btn-outline uppercase border-0 border-b-4">View full menu</button>
+           </div>
         </div>
     );
 };
