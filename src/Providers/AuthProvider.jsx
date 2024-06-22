9f8ec38d9bd317d9fd2f84879e360f68959a0c22
gmnawab <gmnawab25@gmail.com>
2024-06-22 09:56:00 +0600

    created token functionality and insert axios interceptiors

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -2,12 +2,14 @@ import { createContext, useEffect, useState } from "react";
 import auth from "../utilities.jsx/firebase.config";
 import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { GoogleAuthProvider } from "firebase/auth";
+import useAxiosPublic from "../hooks/useAxiosPublic";
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
     const googleProvider = new GoogleAuthProvider();
+    const axiosPublic = useAxiosPublic();
 
     const createUser = (email, password) => {
         setLoading(true);
@@ -30,7 +32,7 @@ const AuthProvider = ({ children }) => {
     }
 
     const updateUser = (name, photo) => {
-      return updateProfile(auth.currentUser, {
+        return updateProfile(auth.currentUser, {
             displayName: name,
             photoURL: photo
         })
@@ -39,6 +41,17 @@ const AuthProvider = ({ children }) => {
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
+            if (currentUser) {
+                const userInfo = { email: currentUser.email }
+                axiosPublic.post('/jwt', userInfo)
+                    .then(res => {
+                        if (res.data.token) {
+                            localStorage.setItem('access-token', res.data.token);
+                        }
+                    })
+            } else {
+                localStorage.removeItem('access-token');
+            }
             setLoading(false);
         })
         return () => {
--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,6 +4,14 @@ const axiosSecure = axios.create({
     baseURL: "http://localhost:5000"
 });
 const useAxiosSecure = () => {
+    axiosSecure.interceptors.request.use(function (config) {
+        const token = localStorage.getItem('access-token');
+        console.log('token from interceptor: ', token);
+        config.headers.authorization = `Bearer ${token}`;
+        return config;
+    }, function (err) {
+        return Promise.reject(err);
+    })
     return axiosSecure;
 };
 
